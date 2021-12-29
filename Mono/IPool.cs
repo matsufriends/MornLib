d6d74c2af6950349ed9244f98eed58ba5f41e0f5
matsu_friends <pikapikan0526@gmail.com>
2021-12-30 00:19:20 +0900

    Poolを追加

---
+++ Mono/IPool.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+namespace MornLib.Mono {
+    public interface IPool<T> where T : MonoBehaviour {
+        public void       PoolDestroy(T pool);
+        public T PoolInstantiate();
+    }
+}
---
+++ Mono/Pool.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+namespace MornLib.Mono {
+    public class Pool<T> : MonoBehaviour,IPool<T> where T : MonoBehaviour {
+        [SerializeField] private T        _prefab;
+        private readonly         Queue<T> _poolList = new Queue<T>();
+        public void PoolDestroy(T pool) {
+            pool.gameObject.SetActive(false);
+            pool.transform.SetParent(null);
+            _poolList.Enqueue(pool);
+        }
+        public T PoolInstantiate() {
+            if(_poolList.Count > 0) {
+                var obj = _poolList.Dequeue();
+                obj.gameObject.SetActive(true);
+                return obj;
+            }
+            return Instantiate(_prefab);
+        }
+    }
+}
