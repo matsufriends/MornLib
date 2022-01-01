c6fcc91905d4e9beadf68029aa2b801c8f5b6084
matsu_friends <pikapikan0526@gmail.com>
2022-01-01 20:10:04 +0900

    protectedをpublicへ

--- Singletons/Singleton.cs
+++ Singletons/Singleton.cs
@@ -1,9 +1,9 @@
 ﻿namespace MornLib.Singletons {
-    public abstract class Singleton<T> where T : Singleton<T>, new() {
+    public abstract class Singleton<T> where T : Singleton<T>,new() {
         private static T s_instance;
-        protected static T Instance {
+        public static T Instance {
             get {
-                if (s_instance != null) return s_instance;
+                if(s_instance != null) return s_instance;
                 s_instance = new T();
                 s_instance.Instanced();
                 return s_instance;
@@ -11,11 +11,11 @@
         }
         protected abstract void Instanced();
     }
-    public abstract class Singleton<TClass, TInterface> where TInterface : ISingleton where TClass : Singleton<TClass, TInterface>, TInterface, new() {
+    public abstract class Singleton<TClass,TInterface> where TInterface : ISingleton where TClass : Singleton<TClass,TInterface>,TInterface,new() {
         private static TInterface s_instance;
         public static TInterface Instance {
             get {
-                if (s_instance != null) return s_instance;
+                if(s_instance != null) return s_instance;
                 s_instance = new TClass();
                 s_instance.Instanced();
                 return s_instance;
