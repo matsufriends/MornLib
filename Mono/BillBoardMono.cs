da1c0e652bcbed468f2192f209ad5d0024731918
matsu_friends <pikapikan0526@gmail.com>
2022-02-17 13:16:39 +0900

    ビルボード表示コンポーネントを実装

---
+++ Mono/BillBoardMono.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+namespace MornLib.Mono {
+    public class BillBoardMono : MonoBehaviour {
+        private Camera _mainCamera;
+        private void Awake() {
+            _mainCamera = Camera.main;
+        }
+        private void Update() {
+            if(_mainCamera == null) _mainCamera        = Camera.main;
+            if(_mainCamera != null) transform.rotation = _mainCamera.transform.rotation;
+        }
+    }
+}
