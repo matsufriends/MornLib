6ec4b2e7679f0d441ba3df63304fb8f4ec8ee90a
matsu_friends <pikapikan0526@gmail.com>
2022-02-23 16:37:21 +0900

    Warningの共同をEditor上で例外処理にした

--- Cores/MornLog.cs
+++ Cores/MornLog.cs
@@ -1,9 +1,12 @@
 ﻿using System;
-using UnityEngine;
 namespace MornLib.Cores {
     public static class MornLog {
         public static void Warning(string text) {
+            #if UNITY_EDITOR
+            throw new Exception(text);
+            #else
             Debug.LogWarning(text);
+            #endif
         }
         public static void Error(string text) {
             throw new Exception(text);
