dfd88df3260eb20e74e1f8f61c97c7ed549d9f97
matsu_friends <pikapikan0526@gmail.com>
2022-03-11 20:24:37 +0900

    Vector2IntにMax用の関数を追加

--- Extensions/Vector2IntEx.cs
+++ Extensions/Vector2IntEx.cs
@@ -7,5 +7,8 @@ namespace MornLib.Extensions {
         public static int Min(this Vector2Int @base) {
             return @base.x < @base.y ? @base.x : @base.y;
         }
+        public static int Max(this Vector2Int @base) {
+            return @base.x < @base.y ? @base.y : @base.x;
+        }
     }
 }
