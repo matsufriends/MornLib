8a2f3e98c8d29769da387465567fbccb75051006
matsu_friends <pikapikan0526@gmail.com>
2022-03-12 20:40:02 +0900

    ライセンス追加　Enumのカウント機能追加

--- Cores/MornEnum.cs
+++ Cores/MornEnum.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 namespace MornLib.Cores {
     public static class MornEnum<T> where T : Enum {
         private static List<T> s_enumList;
+        public static  int     Count => Values().Count;
         public static IReadOnlyList<T> Values() {
             if(s_enumList == null) {
                 s_enumList = new List<T>();
