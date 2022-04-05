c6184286fb49a60da66ee7582df4876257688b14
matsu_friends <pikapikan0526@gmail.com>
2022-04-05 21:08:47 +0900

    Enumをstringで保存するAttributeを追加

---
+++ Editor/EnumToStringAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+namespace MornLib.Editor {
+    public sealed class EnumToStringAttribute : PropertyAttribute {
+        public readonly Type EnumType;
+        public EnumToStringAttribute(Type type) {
+            EnumType = type;
+        }
+    }
+}
