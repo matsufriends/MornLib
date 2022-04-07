2a5be1f5491bf35952706417d63230f01ed8200f
matsu_friends <pikapikan0526@gmail.com>
2022-04-08 02:51:31 +0900

    MornLogの内容を修正

--- Cores/MornLog.cs
+++ Cores/MornLog.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 namespace MornLib.Cores {
     public static class MornLog {
         public static void Log(string text) {
             Debug.Log(text);
         }
         public static void Warning(string text) {
-            #if UNITY_EDITOR
-            Debug.LogError(text);
-            #else
             Debug.LogWarning(text);
-            #endif
         }
         public static void Error(string text) {
-            throw new Exception(text);
+            Debug.LogError(text);
         }
     }
 }
