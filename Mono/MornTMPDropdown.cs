48ba61db0b9c86961d7f948d79f05cc1e0625561
matsu_friends <pikapikan0526@gmail.com>
2022-04-12 00:33:03 +0900

    TMPを追加　TMP_Dropdownの拡張を追加
    左/中/右クリックの反応を決められるように

---
+++ Mono/MornTMPDropdown.cs
@@ -0,0 +1,36 @@
+﻿#if USE_TEXTMESHPRO
+using MornLib.Extensions;
+using TMPro;
+using UnityEngine.EventSystems;
+
+#if UNITY_EDITOR
+using TMPro.EditorUtilities;
+using UnityEditor;
+#endif
+
+namespace MornLib.Mono {
+    public class MornTMPDropdown : TMP_Dropdown {
+        public bool IsClickOnMouseRight;
+        public bool IsClickOnMouseMiddle;
+        public bool IsClickOnMouseLeft;
+        public override void OnPointerClick(PointerEventData eventData) {
+            if(eventData.IsLeftClick() && IsClickOnMouseLeft) Show();
+            if(eventData.IsMiddleClick() && IsClickOnMouseMiddle) Show();
+            if(eventData.IsRightClick() && IsClickOnMouseRight) Show();
+        }
+    }
+    #if UNITY_EDITOR
+    [CustomEditor(typeof(MornTMPDropdown))]
+    public class MornTMPDropdownEditor : DropdownEditor {
+        public override void OnInspectorGUI() {
+            var dropdown = (MornTMPDropdown) target;
+            dropdown.IsClickOnMouseRight = EditorGUILayout.Toggle("IsClickOnMouseRight",dropdown.IsClickOnMouseRight);
+            dropdown.IsClickOnMouseMiddle = EditorGUILayout.Toggle("IsClickOnMouseMiddle",dropdown.IsClickOnMouseMiddle);
+            dropdown.IsClickOnMouseLeft = EditorGUILayout.Toggle("IsClickOnMouseLeft",dropdown.IsClickOnMouseLeft);
+            EditorGUILayout.Space();
+            base.OnInspectorGUI();
+        }
+    }
+    #endif
+}
+#endif
