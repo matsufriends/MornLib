63405a9286c891580d835b91c89e6b16f49a3c34
matsu_friends <pikapikan0526@gmail.com>
2022-04-12 23:59:24 +0900

    PointerEvent追加

---
+++ Mono/IPointerEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UniRx;
+namespace MornLib.Mono {
+    public interface IPointerEvent {
+        public IObservable<Unit> OnPointerEnter { get; }
+        public IObservable<Unit> OnPointerExit { get; }
+        public IObservable<Unit> OnPointerUp { get; }
+        public IObservable<Unit> OnPointerDown { get; }
+        public IObservable<Unit> OnPointerClick { get; }
+    }
+}
