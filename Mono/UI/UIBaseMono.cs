aaa20111a1ec4f0124a2072c2d39178a217988c2
matsu_friends <pikapikan0526@gmail.com>
2022-04-13 01:08:18 +0900

    gameObjectがdisableとなったときにもExitが呼ばれるようにした

--- Mono/UI/UIBaseMono.cs
+++ Mono/UI/UIBaseMono.cs
@@ -5,33 +5,52 @@ using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.EventSystems;
 namespace MornLib.Mono.UI {
-    public class UIBaseMono : MonoBehaviour {
+    public class UIBaseMono : MonoBehaviour,IPointerEvent {
         [SerializeField] private UIBehaviour _ui;
         [SerializeField] private bool _isOnMouseRight;
         [SerializeField] private bool _isOnMouseMiddle;
         [SerializeField] private bool _isOnMouseLeft = true;
+        private bool _isOver;
+        private readonly Subject<Unit> _pointerExitSubject = new Subject<Unit>();
         public IObservable<Unit> OnPointerEnter => _ui.OnPointerEnterAsObservable().Select(_ => Unit.Default);
-        public IObservable<Unit> OnPointerExit => _ui.OnPointerExitAsObservable().Select(_ => Unit.Default);
+        public IObservable<Unit> OnPointerExit => _pointerExitSubject;
         public IObservable<Unit> OnPointerUp => _ui.OnPointerUpAsObservable().Where(Match).Select(_ => Unit.Default);
         public IObservable<Unit> OnPointerDown => _ui.OnPointerDownAsObservable().Where(Match).Select(_ => Unit.Default);
         public IObservable<Unit> OnPointerClick => _ui.OnPointerClickAsObservable().Where(Match).Select(_ => Unit.Default);
         private void Awake() {
-            ;
+            _ui.OnPointerEnterAsObservable().Subscribe(_ => _isOver = true).AddTo(this);
+            _ui.OnPointerExitAsObservable()
+               .Subscribe(
+                    _ => {
+                        _isOver = false;
+                        _pointerExitSubject.OnNext(Unit.Default);
+                    }
+                )
+               .AddTo(this);
+            gameObject.OnDisableAsObservable()
+               .Where(_ => _isOver)
+               .Subscribe(
+                    _ => {
+                        _isOver = false;
+                        _pointerExitSubject.OnNext(Unit.Default);
+                    }
+                )
+               .AddTo(this);
             var users = GetComponents<IUIBaseUser>();
             if(users == null) return;
-            OnPointerEnter.Subscribe(
+            ((IPointerEvent) this).OnPointerEnter.Subscribe(
                     _ => {
                         foreach(var user in users) user.OnSelect();
                     }
                 )
                .AddTo(this);
-            OnPointerExit.Subscribe(
+            ((IPointerEvent) this).OnPointerExit.Subscribe(
                     _ => {
                         foreach(var user in users) user.OnDeSelect();
                     }
                 )
                .AddTo(this);
-            OnPointerClick.Subscribe(
+            ((IPointerEvent) this).OnPointerClick.Subscribe(
                     _ => {
                         foreach(var user in users) user.OnClick();
                     }
