91e4aefdc8ae9ae1056c28112d7b87f1691fa9cb
matsu_friends <pikapikan0526@gmail.com>
2022-05-12 12:23:13 +0900

    Odinをデフォルトでコメントアウト化

--- Singletons/OdinSingletonMono.cs
+++ Singletons/OdinSingletonMono.cs
@@ -1,4 +1,5 @@
-﻿using Sirenix.OdinInspector;
+﻿/*
+using Sirenix.OdinInspector;
 using UnityEngine;
 namespace MornLib.Singletons {
     //     Odin未使用の場合はこのファイル自体を削除してください
@@ -26,3 +27,4 @@ namespace MornLib.Singletons {
         }
     }
 }
+*/
