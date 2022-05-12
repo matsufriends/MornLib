0c1b17bdcd01f40e19c084598c54a354e8a1e34c
matsu_friends <pikapikan0526@gmail.com>
2022-05-12 12:23:38 +0900

    Vector3のRandomを実装

--- Extensions/Vector3Ex.cs
+++ Extensions/Vector3Ex.cs
@@ -4,5 +4,11 @@ namespace MornLib.Extensions {
         public static Vector2 XZ(this Vector3 vector3) {
             return new Vector2(vector3.x, vector3.z);
         }
+        public static Vector3 Random(float value) {
+            var x = UnityEngine.Random.Range(-value,value);
+            var y = UnityEngine.Random.Range(-value,value);
+            var z = UnityEngine.Random.Range(-value,value);
+            return new Vector3(x,y,z);
+        }
     }
 }
