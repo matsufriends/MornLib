9db2c7627ba77211aa7aa2413e10d6318858e304
matsu_friends <pikapikan0526@gmail.com>
2022-06-17 16:15:50 +0900

    SerializeField用のDictionaryを作り直し

---
+++ Cores/MornSerializableDictionaryProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+namespace MornLib.Cores {
+    [System.Serializable]
+    public class MornSerializableDictionaryProvider<TKey,TValue> {
+        [SerializeField] private List<MornKeyValuePair> _list;
+        private Dictionary<TKey,TValue> _dict;
+        public Dictionary<TKey,TValue> GetDictionary() => _dict ??= _list.ToDictionary(pair => pair.Key,pair => pair.Value);
+        [System.Serializable]
+        private struct MornKeyValuePair {
+            public TKey Key;
+            public TValue Value;
+        }
+    }
+}
