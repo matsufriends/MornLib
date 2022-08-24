1eab66c5036d50cc087e5d0c3797f62951fa1e93
matsu_friends <pikapikan0526@gmail.com>
2022-08-24 19:18:06 +0900

    アプリ終了に関するクラス追加

---
+++ Cores/MornApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+namespace MornLib.Cores {
+    public static class MornApp {
+        private static readonly CompositeDisposable s_disposable = new CompositeDisposable();
+        public static ICollection<IDisposable> QuitDisposable => s_disposable;
+        public static void Quit() {
+            s_disposable.Clear();
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            UnityEngine.Application.Quit();
+            #endif
+        }
+    }
+}
