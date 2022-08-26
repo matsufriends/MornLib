38b49af97c7068bec4ee558d02e43ca69daf67d0
matsu_friends <pikapikan0526@gmail.com>
2022-08-26 23:03:40 +0900

    SceneManagerを追加

---
+++ Scenes/MornSceneMono.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+namespace MornLib.Scenes {
+    public abstract class MornSceneMono : MonoBehaviour {
+        protected void HideOwn() {
+            Destroy(gameObject);
+        }
+        protected void HideOwnAndShow(Type sceneType) {
+            HideOwn();
+            Show(sceneType);
+        }
+        protected void Show(Type sceneType) {
+            MornSceneManagerMono.Instance.GenerateScene(sceneType);
+        }
+    }
+}
