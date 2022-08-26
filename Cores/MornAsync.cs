93d6f9e07da435fc619d43479bf20b8dccf0ee39
matsu_friends <pikapikan0526@gmail.com>
2022-08-26 21:55:13 +0900

    MornAsyncの追加

---
+++ Cores/MornAsync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+namespace MornLib.Cores {
+    public static class MornAsync {
+        public static async UniTask TransitionAsync(float time,bool isUnscaledTime,Action<float> action) {
+            if(time <= 0) throw new ArgumentException($"time[{time}]は0以上の値を指定して下さい。");
+            var startTime = isUnscaledTime ? Time.unscaledTime : Time.time;
+            while(true) {
+                var dif = (isUnscaledTime ? Time.unscaledTime : Time.time) - startTime;
+                action(Mathf.Clamp01(dif / time));
+                if(dif >= time) return;
+                await UniTask.NextFrame();
+            }
+        }
+    }
+}
