edb7505f68d9e18bdd82ab35b2696e04fb81259e
matsu_friends <pikapikan0526@gmail.com>
2022-08-31 02:31:26 +0900

    MornMathにラジアンのLerpを追加

--- Cores/MornMath.cs
+++ Cores/MornMath.cs
@@ -1,15 +1,19 @@
 ﻿using System;
+using UnityEngine;
 namespace MornLib.Cores {
     public static class MornMath {
-        public static T MaxBy<T>(T a, T b, Func<T, int> func) {
+        public static T MaxBy<T>(T a,T b,Func<T,int> func) {
             var valueA = func.Invoke(a);
             var valueB = func.Invoke(b);
             return valueA > valueB ? a : b;
         }
-        public static T MinBy<T>(T a, T b, Func<T, int> func) {
+        public static T MinBy<T>(T a,T b,Func<T,int> func) {
             var valueA = func.Invoke(a);
             var valueB = func.Invoke(b);
             return valueA < valueB ? a : b;
+        }
+        public static float LerpRadian(float a,float b,float t) {
+            return Mathf.LerpAngle(a * Mathf.Rad2Deg,b * Mathf.Rad2Deg,t) * Mathf.Deg2Rad;
         }
     }
 }
