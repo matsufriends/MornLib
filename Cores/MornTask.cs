2390c89cadfaed2f77cc0702096c842bc5107cc8
matsu_friends <pikapikan0526@gmail.com>
2022-08-31 02:31:17 +0900

    MoenAsync => MornTaskへ改名

--- Cores/MornTask.cs
+++ Cores/MornTask.cs
@@ -2,7 +2,7 @@
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 namespace MornLib.Cores {
-    public static class MornAsync {
+    public static class MornTask {
         public static async UniTask TransitionAsync(float time,bool isUnscaledTime,Action<float> action) {
             if(time <= 0) throw new ArgumentException($"time[{time}]は0以上の値を指定して下さい。");
             var startTime = isUnscaledTime ? Time.unscaledTime : Time.time;
