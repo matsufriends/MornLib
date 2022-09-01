0cc2be32be61d23f1dd3afe88e0d6936f1df34b4
matsu_friends <pikapikan0526@gmail.com>
2022-09-01 23:22:06 +0900

    FindOnlyOneComponentな機能を追加

---
+++ Cores/MornComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+namespace MornLib.Cores {
+    public static class MornComponent {
+        public static bool TryFindOnlyOneComponent<T>(out T result) where T : MonoBehaviour {
+            var objects = Object.FindObjectsOfType<T>();
+            if(objects.Length == 1) {
+                result = objects[0];
+                return true;
+            }
+            result = null;
+            return false;
+        }
+    }
+}
