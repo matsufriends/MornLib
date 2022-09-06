ff38a3331e45ee3c67675750254e6f1881c2d8c1
matsu_friends <pikapikan0526@gmail.com>
2022-09-06 10:01:21 +0900

    MornStateMachineの追加

---
+++ Cores/MornSM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+namespace MornLib.Cores {
+    public sealed class MornSm<TEnum,TArg> where TEnum : Enum {
+        private readonly Dictionary<TEnum,Action<TArg>> _taskDictionary = new();
+        private TEnum _curState;
+        private float _startTime;
+        private readonly bool _isUnScaledTime;
+        public bool IsFirst { get; private set; }
+        public float PlayingTime => (_isUnScaledTime ? Time.unscaledTime : Time.time) - _startTime;
+        public MornSm(TEnum initType,bool isUnscaledTime) {
+            _isUnScaledTime = isUnscaledTime;
+            ChangeState(initType);
+        }
+        public void Handle(TArg arg) {
+            var cacheState = _curState;
+            _taskDictionary[_curState].Invoke(arg);
+            if(EqualityComparer<TEnum>.Default.Equals(cacheState,_curState)) IsFirst = false;
+        }
+        public void RegisterState(TEnum type,Action<TArg> task) {
+            _taskDictionary.Add(type,task);
+        }
+        public void ChangeState(TEnum type) {
+            _curState = type;
+            try {
+                _startTime = _isUnScaledTime ? Time.unscaledTime : Time.time;
+            } catch {
+                _startTime = 0;
+            }
+            IsFirst = true;
+        }
+        public bool IsState(TEnum type) {
+            return EqualityComparer<TEnum>.Default.Equals(_curState,type);
+        }
+    }
+}
