b92e4706f1ca71fc76b503b28db4265e7e67c039
matsu_friends <pikapikan0526@gmail.com>
2022-09-08 01:50:37 +0900

    Clampの実装

--- Cores/MornFloatRange.cs
+++ Cores/MornFloatRange.cs
@@ -6,10 +6,16 @@ namespace MornLib.Cores {
         public float End;
         public MornFloatRange(float start,float end) {
             Start = start;
-            End = end;
+            End   = end;
         }
         public float Lerp(float rate) {
             return Mathf.Lerp(Start,End,Mathf.Clamp01(rate));
+        }
+        public float Clamp(float value) {
+            return Mathf.Clamp(value,Start,End);
+        }
+        public float Random() {
+            return UnityEngine.Random.Range(Start,End);
         }
     }
 }
