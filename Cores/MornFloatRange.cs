32fcc977697b3a2e46d2c1e47f744537f2cab969
matsu_friends <pikapikan0526@gmail.com>
2022-09-08 00:12:54 +0900

    Vector2とfloatのLerp用structの追加

---
+++ Cores/MornFloatRange.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace MornLib.Cores {
+    [System.Serializable]
+    public struct MornFloatRange {
+        public float Start;
+        public float End;
+        public MornFloatRange(float start,float end) {
+            Start = start;
+            End = end;
+        }
+        public float Lerp(float rate) {
+            return Mathf.Lerp(Start,End,Mathf.Clamp01(rate));
+        }
+    }
+}
---
+++ Cores/MornVector2Range.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace MornLib.Cores {
+    [System.Serializable]
+    public struct MornVector2Range {
+        public Vector2 Start;
+        public Vector2 End;
+        public MornVector2Range(Vector2 start,Vector2 end) {
+            Start = start;
+            End = end;
+        }
+        public Vector2 Lerp(float rate) {
+            return Vector2.Lerp(Start,End,Mathf.Clamp01(rate));
+        }
+    }
+}
