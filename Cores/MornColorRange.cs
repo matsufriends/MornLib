99bfbcdc00477f3d41d0b79d5fb5f0f9f406754a
matsu_friends <pikapikan0526@gmail.com>
2022-09-08 23:02:08 +0900

    ColorRangeを追加

---
+++ Cores/MornColorRange.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+namespace MornLib.Cores {
+    [System.Serializable]
+    public struct MornColorRange {
+        public Color Start;
+        public Color End;
+        public MornColorRange(Color start,Color end) {
+            Start = start;
+            End = end;
+        }
+        public Color Lerp(float rate) {
+            return Color.Lerp(Start,End,Mathf.Clamp01(rate));
+        }
+    }
+}
