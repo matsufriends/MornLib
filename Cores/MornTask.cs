79e3d9116a6ca99a832106a4148f399dfa16f106
matsu_friends <pikapikan0526@gmail.com>
2022-09-08 23:02:01 +0900

    TransitionにCancellationTokenを追加

--- Cores/MornTask.cs
+++ Cores/MornTask.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 namespace MornLib.Cores {
     public static class MornTask {
-        public static async UniTask TransitionAsync(TimeSpan duration,bool isUnscaledTime,Action<float> action) {
+        public static async UniTask TransitionAsync(TimeSpan duration,bool isUnscaledTime,Action<float> action,CancellationToken token) {
             var time = (float) duration.TotalSeconds;
             if(time <= 0) throw new ArgumentException($"durationは0以上の値を指定して下さい。");
             var startTime = isUnscaledTime ? Time.unscaledTime : Time.time;
@@ -11,7 +12,7 @@ namespace MornLib.Cores {
                 var dif = (isUnscaledTime ? Time.unscaledTime : Time.time) - startTime;
                 action(Mathf.Clamp01(dif / time));
                 if(dif >= time) return;
-                await UniTask.NextFrame();
+                await UniTask.NextFrame(token);
             }
         }
     }
