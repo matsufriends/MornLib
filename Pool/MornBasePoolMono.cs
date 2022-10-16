bc8546fd28b13c99c481e5f875bbe76a076f75cd
matsu_friends <pikapikan0526@gmail.com>
2022-10-17 08:15:26 +0900

    MornBasePoolの追加

---
+++ Pool/MornBasePoolMono.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace MornLib.Pool {
+    public sealed class MornObjectPool<T> {
+        private readonly Stack<T> _poolStack = new();
+        private readonly Func<T> _generate;
+        private readonly Action<T> _onPop;
+        private readonly Action<T> _onPush;
+        public MornObjectPool(Func<T> generate,Action<T> onPop,Action<T> onPush,uint startCount) {
+            _generate = generate;
+            _onPop      = onPop;
+            _onPush     = onPush;
+            for(var i = 0;i < startCount;i++) {
+                var generated = generate();
+                onPush(generated);
+                _poolStack.Push( generated);
+            }
+        }
+        public T Pop() {
+            if(_poolStack.TryPop(out var result) == false) result =_generate();
+            _onPop(result);
+            return result;
+        }
+        public void Push(T pushObject) {
+            _onPush(pushObject);
+            _poolStack.Push(pushObject);
+        }
+    }
+}
