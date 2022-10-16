81511ab65390eb8fbd5b21e00286713f67e253e2
matsu_friends <pikapikan0526@gmail.com>
2022-10-17 08:15:16 +0900

    TaskCancellerの追加

---
+++ Cores/MornTaskCanceller.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+namespace MornLib.Cores {
+    public class MornTaskCanceller {
+        private CancellationTokenSource _source = new();
+        private readonly CancellationToken _onDestroyToken;
+        public CancellationToken Token { get; private set; }
+        public MornTaskCanceller(GameObject gameObject) {
+            _onDestroyToken = gameObject.GetCancellationTokenOnDestroy();
+            Token           = CancellationTokenSource.CreateLinkedTokenSource(_onDestroyToken,_source.Token).Token;
+        }
+        public void Cancel() {
+            _source.Cancel();
+            _source.Dispose();
+            _source = new CancellationTokenSource();
+            Token   = CancellationTokenSource.CreateLinkedTokenSource(_onDestroyToken,_source.Token).Token;
+        }
+    }
+}
