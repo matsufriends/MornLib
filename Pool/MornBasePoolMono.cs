d4efab8db9f5070ca6a61db52783683bf59ee188
matsu_friends <pikapikan0526@gmail.com>
2022-10-24 10:06:15 +0900

    MornPoolの変数名のリファクタ

--- Pool/MornBasePoolMono.cs
+++ Pool/MornBasePoolMono.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 namespace MornLib.Pool {
     public sealed class MornObjectPool<T> {
         private readonly Stack<T> _poolStack = new();
-        private readonly Func<T> _generate;
+        private readonly Func<T> _generator;
         private readonly Action<T> _onPop;
         private readonly Action<T> _onPush;
-        public MornObjectPool(Func<T> generate,Action<T> onPop,Action<T> onPush,uint startCount) {
-            _generate = generate;
+        public MornObjectPool(Func<T> generator,Action<T> onPop,Action<T> onPush,uint startCount) {
+            _generator = generator;
             _onPop      = onPop;
             _onPush     = onPush;
             for(var i = 0;i < startCount;i++) {
-                var generated = generate();
+                var generated = generator();
                 onPush(generated);
                 _poolStack.Push( generated);
             }
         }
         public T Pop() {
-            if(_poolStack.TryPop(out var result) == false) result =_generate();
+            if(_poolStack.TryPop(out var result) == false) result =_generator();
             _onPop(result);
             return result;
         }
