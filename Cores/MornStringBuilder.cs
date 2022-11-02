611394012449cecd2eb9cf9cbb71bb90af119f65
matsu_friends <pikapikan0526@gmail.com>
2022-11-03 02:10:27 +0900

    MornStringBuilderを使い回せる仕様に修正した

--- Cores/MornStringBuilder.cs
+++ Cores/MornStringBuilder.cs
@@ -3,14 +3,14 @@ using MornLib.Pool;
 namespace MornLib.Cores {
     public sealed class MornStringBuilder : IPoolItem {
         private readonly StringBuilder _builder = new();
-        private readonly char _split;
         private bool _isFirst;
-        public MornStringBuilder(char split) {
+        private char _split;
+        public void Clear() => _builder.Clear();
+        public void Init(char split) {
             _builder.Clear();
             _split   = split;
             _isFirst = true;
         }
-        public void Clear() => _builder.Clear();
         public void Append(string message) {
             if(_isFirst == false) _builder.Append(_split);
             _builder.Append(message);
