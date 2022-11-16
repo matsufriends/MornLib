7f8a9923a4f21f26f99afe713ab63720cab18452
matsu_friends <pikapikan0526@gmail.com>
2022-11-16 12:58:52 +0900

    2次元配列をラップするGrid型を追加

---
+++ Grid/Grid.cs
@@ -0,0 +1,26 @@
+﻿namespace MornLib.Grid {
+    public class Grid<T> : IGridReader<T> {
+        private readonly T[,] _board;
+        public int Width { get; }
+        public int Height { get; }
+        public T this[int x,int y] {
+            get => _board[x,y];
+            set => _board[x,y] = value;
+        }
+        public Grid(int width,int height) {
+            Width  = width;
+            Height = height;
+            _board = new T[Width,Height];
+        }
+        public bool TrtGet(int x,int y,out T value) {
+            var isValid = 0 <= x && x < Width && 0 <= y && y < Height;
+            value = isValid ? _board[x,y] : default;
+            return isValid;
+        }
+        public bool TrtSet(int x,int y,T value) {
+            var isValid = 0 <= x && x < Width && 0 <= y && y < Height;
+            if(isValid) _board[x,y] = value;
+            return isValid;
+        }
+    }
+}
---
+++ Grid/IGridReader.cs
@@ -0,0 +1,9 @@
+﻿namespace MornLib.Grid {
+    public interface IGridReader<T> {
+        public int Width { get; }
+        public int Height { get; }
+        public T this[int      x,int y] { get; }
+        public bool TrtGet(int x,int y,out T value);
+        public bool TrtSet(int x,int y,T     value);
+    }
+}
