561db0d073c5a4ea6c441b8c72504a6fb1bfebd3
matsu_friends <pikapikan0526@gmail.com>
2022-12-01 19:55:42 +0900

    fix: Scene管理用の汎用的クラス追加

---
+++ Scenes/BaseSceneControllerMono.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+namespace MornLib.Scenes
+{
+    public abstract class BaseSceneControllerMono<TEnum> : MonoBehaviour where TEnum : Enum
+    {
+        [SerializeField] private List<BaseSceneMono<TEnum>> _sceneList;
+
+        private readonly Dictionary<TEnum, BaseSceneMono<TEnum>> _sceneDictionary = new();
+        private TEnum _sceneType;
+
+        private void Awake()
+        {
+            foreach (var scene in _sceneList)
+            {
+                _sceneDictionary.Add(scene.SceneType, scene);
+                scene.OnChangeScene.Subscribe(ChangeScene)
+                    .AddTo(this);
+            }
+        }
+
+        private void ChangeScene(TEnum sceneType)
+        {
+            _sceneDictionary[_sceneType].OnExitScene();
+            _sceneType = sceneType;
+            _sceneDictionary[_sceneType].OnEnterScene();
+        }
+
+        public void MyUpdate()
+        {
+            _sceneDictionary[_sceneType].SceneUpdate();
+        }
+    }
+}
---
+++ Scenes/BaseSceneMono.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace MornLib.Scenes
+{
+    public abstract class BaseSceneMono<TEnum> : MonoBehaviour where TEnum : Enum
+    {
+        [SerializeField] private TEnum _sceneType;
+        private readonly Subject<TEnum> _changeSceneSubject = new();
+        public TEnum SceneType => _sceneType;
+        public IObservable<TEnum> OnChangeScene => _changeSceneSubject;
+
+        protected void ChangeScene(TEnum sceneType)
+        {
+            _changeSceneSubject.OnNext(sceneType);
+        }
+
+        public abstract void OnEnterScene();
+        public abstract void SceneUpdate();
+        public abstract void OnExitScene();
+    }
+}
