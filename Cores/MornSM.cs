d5adc85d6e696d111962f50e4c5783965731ac8c
matsu_friends <pikapikan0526@gmail.com>
2022-12-05 23:36:29 +0900

    fix: MornSmの現在のStateを参照できるよう修正

--- Cores/MornSM.cs
+++ Cores/MornSM.cs
@@ -7,17 +7,17 @@ namespace MornLib.Cores
     public sealed class MornSm<TEnum, TArg> where TEnum : Enum
     {
         private readonly Dictionary<TEnum, Action<TArg>> _taskDictionary = new();
-        private TEnum _curState;
         private float _startTime = -1;
         private bool _isStateChanged;
         private readonly bool _isUnScaledTime;
+        public TEnum CurState { get; private set; }
         public bool IsFirst { get; private set; }
         public float PlayingTime => (_isUnScaledTime ? Time.unscaledTime : Time.time) - _startTime;
 
         public MornSm(TEnum initType, bool isUnscaledTime = false)
         {
             _isUnScaledTime = isUnscaledTime;
-            _curState = initType;
+            CurState = initType;
             IsFirst = true;
             _isStateChanged = false;
         }
@@ -29,7 +29,7 @@ namespace MornLib.Cores
                 _startTime = _isUnScaledTime ? Time.unscaledTime : Time.time;
             }
 
-            _taskDictionary[_curState].Invoke(arg);
+            _taskDictionary[CurState].Invoke(arg);
             if (_isStateChanged == false)
             {
                 IsFirst = false;
@@ -45,7 +45,7 @@ namespace MornLib.Cores
 
         public void ChangeState(TEnum type)
         {
-            _curState = type;
+            CurState = type;
             _startTime = _isUnScaledTime ? Time.unscaledTime : Time.time;
             IsFirst = true;
             _isStateChanged = true;
@@ -53,7 +53,7 @@ namespace MornLib.Cores
 
         public bool IsState(TEnum type)
         {
-            return EqualityComparer<TEnum>.Default.Equals(_curState, type);
+            return EqualityComparer<TEnum>.Default.Equals(CurState, type);
         }
     }
 }
