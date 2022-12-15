cfa4f0a35323637be5ad68ee9d384769bdb73466
matsu_friends <pikapikan0526@gmail.com>
2022-12-16 02:09:08 +0900

    feat: MornSingletonSoをResourcesフォルダに作成するよう修正

--- Singletons/MornSingletonSo.cs
+++ Singletons/MornSingletonSo.cs
@@ -18,26 +18,19 @@ namespace MornLib.Singletons
                     return s_instance;
                 }
 
-                var objects = Resources.FindObjectsOfTypeAll<T>();
-                switch (objects.Length)
+                s_instance = Resources.Load<T>(typeof(T).Name);
+                if (s_instance != null)
                 {
-                    case 0:
-                        s_instance = CreateInstance<T>();
-#if UNITY_EDITOR
-                        var fileName = $"{typeof(T).Name}.asset";
-                        var path = Path.Combine("Assets", fileName);
-                        AssetDatabase.CreateAsset(s_instance, path);
-                        MornLog.Log($"SingletonSo:{fileName}を{path}に作成しました");
-#endif
-                        break;
-                    case 1:
-                        s_instance = objects[0];
-                        break;
-                    default:
-                        MornLog.Error($"{typeof(T).Name}が{objects.Length}つ存在します");
-                        break;
+                    return s_instance;
                 }
 
+                s_instance = CreateInstance<T>();
+#if UNITY_EDITOR
+                var fileName = $"{typeof(T).Name}.asset";
+                var path = Path.Combine("Assets/Resources", fileName);
+                AssetDatabase.CreateAsset(s_instance, path);
+                MornLog.Log($"SingletonSo:{fileName}を{path}に作成しました");
+#endif
                 return s_instance;
             }
         }
