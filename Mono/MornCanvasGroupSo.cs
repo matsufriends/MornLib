1074e22fb4e5000d621a5e84366d987718970f6d
matsu_friends <pikapikan0526@gmail.com>
2023-01-08 17:06:44 +0900

    fix:CanvasSetterのInpectorにボタンを追加

---
+++ Mono/MornCanvasGroupSo.cs
@@ -0,0 +1,14 @@
+﻿using MornLib.Singletons;
+using UnityEngine;
+
+namespace MornLib.Mono
+{
+    [CreateAssetMenu(fileName = nameof(MornCanvasGroupSo), menuName = nameof(MornCanvasGroupSo))]
+    public class MornCanvasGroupSo : MornSingletonSo<MornCanvasGroupSo>
+    {
+        [SerializeField] private int _width;
+        [SerializeField] private int _height;
+        public int Width => _width;
+        public int Height => _height;
+    }
+}
