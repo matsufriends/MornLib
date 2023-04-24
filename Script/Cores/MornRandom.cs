04c31c0d4dbae942df360dcb988811ea786e138f
matsu_friends <pikapikan0526@gmail.com>
2023-04-25 00:05:57 +0900

    [feat] MornRandom追加

---
+++ Script/Cores/MornRandom.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using MornLib.Extensions;
+
+namespace MornLib.Cores
+{
+    public static class MornRandom
+    {
+        public static T Random<T>(params T[] args)
+        {
+            return args.RandomValue();
+        }
+
+        public static T Random<T>(params (T, float)[] args)
+        {
+            var random = UnityEngine.Random.value * args.Sum(pair => pair.Item2);
+            foreach (var pair in args)
+            {
+                if (pair.Item2 < random)
+                {
+                    return pair.Item1;
+                }
+            }
+
+            return args[^1].Item1;
+        }
+    }
+}
