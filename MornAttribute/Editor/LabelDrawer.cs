d19d5e36bee8fce9a737249b2fe3ed2ca4e1dc08
matsu_friends <pikapikan0526@gmail.com>
2023-04-28 01:24:21 +0900

    [fix] LabelAttributeの高さが計算されないバグ修正

--- MornAttribute/Editor/LabelDrawer.cs
+++ MornAttribute/Editor/LabelDrawer.cs
@@ -9,7 +9,12 @@ namespace MornAttribute
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var labelAttribute = (LabelAttribute)attribute;
-            EditorGUI.PropertyField(position, property, new GUIContent(labelAttribute.LabelName));
+            EditorGUI.PropertyField(position, property, new GUIContent(labelAttribute.LabelName), true);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, true);
         }
     }
 }
