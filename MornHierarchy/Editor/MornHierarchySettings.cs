0b1e3135afa995e948a6632ba331fa671ce0c5de
matsu_friends <pikapikan0526@gmail.com>
2023-05-08 23:01:43 +0900

    [fix] MornHierarchyのshowTagをデフォルト非表示にした

--- MornHierarchy/Editor/MornHierarchySettings.cs
+++ MornHierarchy/Editor/MornHierarchySettings.cs
@@ -6,8 +6,8 @@ namespace MornHierarchy
     [FilePath(nameof(MornHierarchySettings) + ".asset", FilePathAttribute.Location.PreferencesFolder)]
     internal sealed class MornHierarchySettings : ScriptableSingleton<MornHierarchySettings>
     {
-        [SerializeField] private bool _showTag = true;
-        [SerializeField, Range(0, 1)] private float _transparent = 0.3f;
+        [SerializeField] private bool _showTag;
+        [SerializeField] [Range(0, 1)] private float _transparent = 0.3f;
         public bool ShowTag => _showTag;
         public float Transparent => _transparent;
 
