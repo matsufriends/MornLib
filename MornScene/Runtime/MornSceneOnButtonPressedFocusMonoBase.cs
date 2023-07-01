84aa15197a7a023e1d18fb3ab75ede2ee637c55f
matsu_friends <pikapikan0526@gmail.com>
2023-07-01 22:46:50 +0900

    [feat] ボタン押下時に自動フォーカスするコンポーネント追加

---
+++ MornScene/Runtime/MornSceneOnButtonPressedFocusMonoBase.cs
@@ -0,0 +1,31 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace MornScene
+{
+    [RequireComponent(typeof(MornSceneMonoBase))]
+    public abstract class MornSceneOnButtonPressedFocusMonoBase : MonoBehaviour
+    {
+        [SerializeField] private MornSceneMonoBase _scene;
+        [SerializeField] private GameObject _focusObject;
+        protected abstract bool OnBackButtonPressed { get; }
+
+        private void Reset()
+        {
+            _scene = GetComponent<MornSceneMonoBase>();
+        }
+
+        private void Awake()
+        {
+            _scene.OnUpdateSceneRx.Subscribe(
+                _ =>
+                {
+                    if (OnBackButtonPressed)
+                    {
+                        EventSystem.current.SetSelectedGameObject(_focusObject);
+                    }
+                }).AddTo(this);
+        }
+    }
+}
