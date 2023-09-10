384b1606a50dbebfc82c4a601ef36c1849bbbb6b
まっともぉん <50489724+matsufriends@users.noreply.github.com>
2023-09-10 23:37:13 +0900

    [feat] MornWebの追加

---
+++ MornWeb/Runtime/MornWebOpenURLButton.cs
@@ -0,0 +1,23 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MornLib
+{
+    [RequireComponent(typeof(Button))]
+    public class MornWebOpenURLButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private string _url;
+
+        private void Awake()
+        {
+            _button.OnClickAsObservable().Subscribe(_ => MornWebUtil.Open(_url)).AddTo(this);
+        }
+
+        private void Reset()
+        {
+            _button = GetComponent<Button>();
+        }
+    }
+}
---
+++ MornWeb/Runtime/MornWebUtil.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace MornLib
+{
+    public static class MornWebUtil
+    {
+        [DllImport("__Internal")]
+        private static extern void OpenWindow(string url);
+
+        public static void Open(string url)
+        {
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                OpenWindow(url);
+            }
+            else
+            {
+#if UNITY_EDITOR
+                Process.Start(url);
+#else
+				UnityEngine.Debug.Log ("WebGL以外では実行できません。");
+				UnityEngine.Debug.Log (url);
+#endif
+            }
+        }
+    }
+}
