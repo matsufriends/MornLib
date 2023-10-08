3bd95a38bd3ca5fa883b73f97b549a9794ccbc1b
matsu_friends <pikapikan0526@gmail.com>
2023-10-09 07:06:06 +0900

    [feat] Editor拡張を簡易化するMornEditorの追加

---
+++ MornEditor/Editor/MornEditorWindow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace MornEditor
+{
+    public sealed class MornEditorWindow : EditorWindow
+    {
+        private Vector2 _scroll;
+
+        [MenuItem("MornLib/MornEditorWindow")]
+        private static void ShowWindow()
+        {
+            GetWindow<MornEditorWindow>();
+        }
+
+        private void OnGUI()
+        {
+            MornEditorCore.CheckDisposed();
+            using (var scrollScope = new EditorGUILayout.ScrollViewScope(_scroll))
+            {
+                foreach (var info in MornEditorCore.OnGUIHashSet)
+                {
+                    GUILayout.Label(info.Label);
+                    info.OnGUI();
+                    GUILayout.Space(10);
+                }
+
+                _scroll = scrollScope.scrollPosition;
+            }
+        }
+    }
+}
---
+++ MornEditor/Runtime/MornEditorOnGUIData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MornEditor
+{
+    public sealed class MornEditorOnGUIData : IDisposable
+    {
+        public readonly string Label;
+        public readonly Action OnGUI;
+        public bool IsDisposed { get; private set; }
+
+        public MornEditorOnGUIData(string label, Action onGUI)
+        {
+            Label = label;
+            OnGUI = onGUI;
+        }
+
+        void IDisposable.Dispose()
+        {
+            IsDisposed = true;
+        }
+    }
+}
