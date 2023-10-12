0987ddd4f1b80826af7c566e02c4c3db2243ac5a
matsu_friends <pikapikan0526@gmail.com>
2023-10-12 20:30:33 +0900

    [feat] .metaファイルの更新

--- MornScene/Runtime/MornSceneController.cs
+++ MornScene/Runtime/MornSceneController.cs
@@ -3,41 +3,43 @@ using UnityEngine;
 
 namespace MornScene
 {
-    public static class MornSceneCore
+    public sealed class MornSceneController
     {
-        private static readonly List<MornSceneDataSo> _sceneUpdateList = new();
-        private static readonly List<MornSceneDataSo> _cachedUpdateList = new();
+        private readonly MornSceneSolver _sceneSolver;
+        private readonly List<MornSceneDataSo> _sceneUpdateList = new();
+        private readonly List<MornSceneDataSo> _cachedUpdateList = new();
 
-        internal static void Reset()
+        public MornSceneController(MornSceneSolver sceneSolver)
+        {
+            _sceneSolver = sceneSolver;
+        }
+        
+        internal void Reset()
         {
             _sceneUpdateList.Clear();
             _cachedUpdateList.Clear();
         }
 
-        internal static void ChangeScene(MornSceneDataSo sceneDataSo)
+        internal void ChangeScene(MornSceneDataSo sceneDataSo)
         {
-            var solver = MornSceneSolver.Instance;
             foreach (var updateScene in _sceneUpdateList)
             {
-                solver[updateScene].OnExitScene(updateScene);
+                _sceneSolver[updateScene].OnExitScene(updateScene);
             }
 
             _sceneUpdateList.Clear();
             AddScene(sceneDataSo);
         }
 
-        internal static void AddScene(MornSceneDataSo sceneData)
+        internal void AddScene(MornSceneDataSo sceneData)
         {
-            var solver = MornSceneSolver.Instance;
-            solver[sceneData].OnEnterScene(sceneData);
+            _sceneSolver[sceneData].OnEnterScene(sceneData);
             _sceneUpdateList.Add(sceneData);
         }
 
-        internal static void RemoveScene(MornSceneDataSo sceneData)
+        internal void RemoveScene(MornSceneDataSo sceneData)
         {
-            var solver = MornSceneSolver.Instance;
-            var scene = solver[sceneData];
-            scene.OnExitScene(sceneData);
+            _sceneSolver[sceneData].OnExitScene(sceneData);
             var sceneName = sceneData.SceneName;
             if (_sceneUpdateList.Count > 0)
             {
@@ -56,15 +58,14 @@ namespace MornScene
             }
         }
 
-        internal static void UpdateScene()
+        internal void UpdateScene()
         {
-            var solver = MornSceneSolver.Instance;
             _cachedUpdateList.Clear();
             _cachedUpdateList.AddRange(_sceneUpdateList);
             for (var i = 0; i < _cachedUpdateList.Count; i++)
             {
                 var sceneData = _cachedUpdateList[i];
-                solver[sceneData].OnUpdateScene(sceneData, i == _cachedUpdateList.Count - 1);
+                _sceneSolver[sceneData].OnUpdateScene(sceneData, i == _cachedUpdateList.Count - 1);
             }
         }
     }
