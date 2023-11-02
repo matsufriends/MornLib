c81b8aa33ab34ab86460caea73d4e9fb50bbc1a8
matsu_friends <pikapikan0526@gmail.com>
2023-11-02 20:16:44 +0900

    [feat] MornSceneObjectの追加

---
+++ MornScene/Runtime/MornSceneObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace MornScene
+{
+    [Serializable]
+    public class MornSceneObject
+    {
+        [SerializeField] private string _sceneName;
+
+        public static implicit operator string(MornSceneObject mornSceneObject)
+        {
+            return mornSceneObject._sceneName;
+        }
+
+        public static implicit operator MornSceneObject(string sceneName)
+        {
+            return new MornSceneObject
+            {
+                _sceneName = sceneName,
+            };
+        }
+    }
+}
