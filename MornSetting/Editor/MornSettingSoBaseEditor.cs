7195a451ce679b8be2bfc618707da1652ed90dde
matsu_friends <pikapikan0526@gmail.com>
2023-11-04 23:36:42 +0900

    [feat] MornSettingのEditor拡張

---
+++ MornSetting/Editor/MornSettingSoBaseEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace MornSetting.MornLib.MornSetting.Editor
+{
+    [CustomEditor(typeof(MornSettingSoBase), true)] [CanEditMultipleObjects]
+    internal sealed class MornSettingSoBaseEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("File Name -> Scene Name"))
+            {
+                foreach (var target in targets)
+                {
+                    var settingSo = (MornSettingSoBase)target;
+                    var assetPath = AssetDatabase.GetAssetPath(settingSo);
+                    var fileName = Path.GetFileNameWithoutExtension(assetPath);
+                    settingSo.SetKey(fileName);
+                    EditorUtility.SetDirty(settingSo);
+                }
+            }
+        }
+    }
+}
--- MornSetting/Runtime/MornSettingSoBase.cs
+++ MornSetting/Runtime/MornSettingSoBase.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
 
+[assembly: InternalsVisibleTo("MornSetting.Editor")]
 namespace MornSetting
 {
     public abstract class MornSettingSoBase : ScriptableObject
     {
         [SerializeField] private string _key;
         protected string Key => _key;
+
+        internal void SetKey(string key)
+        {
+            _key = key;
+        }
     }
 }
