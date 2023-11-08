3dee3cfb5239b7a8585e13fe3b79ec6477a2e83c
matsu_friends <pikapikan0526@gmail.com>
2023-11-09 02:36:32 +0900

    [refactor] Editorコードの分離

--- MornUI/Runtime/MornUIVisibilityMonoBase.cs
+++ MornUI/Runtime/MornUIVisibilityMonoBase.cs
@@ -1,7 +1,4 @@
 ﻿using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 namespace MornUI
 {
@@ -10,33 +7,4 @@ namespace MornUI
         public abstract void Show(bool immediate = false);
         public abstract void Hide(bool immediate = false);
     }
-
-#if UNITY_EDITOR
-    [CustomEditor(typeof(MornUIVisibilityMonoBase), true)]
-    public sealed class MornUIVisibilityMoveEditor : Editor
-    {
-        private MornUIVisibilityMonoBase _visibility;
-
-        private void OnEnable()
-        {
-            _visibility = (MornUIVisibilityMonoBase)target;
-        }
-
-        public override void OnInspectorGUI()
-        {
-            base.OnInspectorGUI();
-            if (GUILayout.Button("Show"))
-            {
-                _visibility.Show(true);
-                EditorUtility.SetDirty(_visibility);
-            }
-
-            if (GUILayout.Button("Hide"))
-            {
-                _visibility.Hide(true);
-                EditorUtility.SetDirty(_visibility);
-            }
-        }
-    }
-#endif
 }
