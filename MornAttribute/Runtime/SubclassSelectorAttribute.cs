7c74e5efbd95725268a25f4d31f6616af40408b8
matsu_friends <pikapikan0526@gmail.com>
2023-11-13 18:38:05 +0900

    [feat] SubclassSelector

---
+++ MornAttribute/Runtime/SubclassSelectorAttribute.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace MornAttribute
+{
+    public sealed class SubclassSelectorAttribute : PropertyAttribute
+    {
+        private readonly bool _includeMono;
+
+        public SubclassSelectorAttribute(bool includeMono = false)
+        {
+            _includeMono = includeMono;
+        }
+
+        public bool IsIncludeMono()
+        {
+            return _includeMono;
+        }
+    }
+}
