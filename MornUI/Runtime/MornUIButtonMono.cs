3ca6b84239cc7a78c83c0d56f5629b3d92f726e9
matsu_friends <pikapikan0526@gmail.com>
2023-11-17 01:32:23 +0900

    [feat] 複数スケール対応

--- MornUI/Runtime/MornUIButtonMono.cs
+++ MornUI/Runtime/MornUIButtonMono.cs
@@ -9,8 +9,15 @@ namespace MornUI
         [SerializeField] private GameObject _focused;
         [SerializeField] private GameObject _unfocused;
         private readonly Subject<Unit> _onSubmitAsObservableSubject = new();
+        private Vector3 _defaultScale;
+        
         private const float SubmitScale = 1.1f;
         private const float LerpT = 0.1f;
+
+        private void Awake()
+        {
+            _defaultScale = RectTransform.localScale;
+        }
 
         public IObservable<Unit> OnSubmitAsObservable()
         {
@@ -21,7 +28,7 @@ namespace MornUI
         {
             base.OnSubmit();
             _onSubmitAsObservableSubject.OnNext(Unit.Default);
-            RectTransform.localScale = Vector3.one * SubmitScale;
+            RectTransform.localScale = _defaultScale * SubmitScale;
         }
 
         public override void OnFocus()
@@ -38,7 +45,7 @@ namespace MornUI
 
         private void Update()
         {
-            RectTransform.localScale = Vector3.Lerp(RectTransform.localScale, Vector3.one, LerpT);
+            RectTransform.localScale = Vector3.Lerp(RectTransform.localScale, _defaultScale, LerpT);
         }
     }
 }
