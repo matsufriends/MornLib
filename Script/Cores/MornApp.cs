39985695b0bd78b255c13ec59e87cf94fdb9e72e
matsu_friends <pikapikan0526@gmail.com>
2023-12-30 11:06:41 +0900

    [feat] AppQuitにToken追加

--- Script/Cores/MornApp.cs
+++ Script/Cores/MornApp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using UniRx;
 using UnityEditor;
 
@@ -7,12 +8,16 @@ namespace MornLib.Cores
 {
     public static class MornApp
     {
+        private static readonly CancellationTokenSource s_tokenSource = new();
         private static readonly CompositeDisposable s_disposable = new();
+        public static CancellationToken QuitToken => s_tokenSource.Token;
         public static ICollection<IDisposable> QuitDisposable => s_disposable;
+        
 
         public static void Quit()
         {
             s_disposable.Clear();
+            s_tokenSource.Cancel();
 #if UNITY_EDITOR
             EditorApplication.isPlaying = false;
 #else
