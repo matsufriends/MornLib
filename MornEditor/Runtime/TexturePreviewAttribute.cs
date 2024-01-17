4323d927193e0a55ea1099ceaa1c529424119bb6
matsu_friends <pikapikan0526@gmail.com>
2024-01-17 10:04:48 +0900

    [feat] TexturePreviewの追加

---
+++ MornEditor/Runtime/TexturePreviewAttribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace MornEditor
+{
+    public sealed class TexturePreviewAttribute : PropertyAttribute
+    {
+        public readonly float Size;
+
+        public TexturePreviewAttribute(float size = 200)
+        {
+            Size = size;
+        }
+    }
+}
