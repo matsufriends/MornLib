615aeafd66e72b4f7f1ddfe1d8f7118a0b1707d4
matsu_friends <pikapikan0526@gmail.com>
2024-06-02 01:53:26 +0900

    [feat] TimeSpanの拡張

---
+++ Script/Cores/MornTimeSpan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MornLib.Cores
+{
+    public static class MornTimeSpan
+    {
+        public static TimeSpan ToTimeSpanAsSeconds(this float seconds)
+        {
+            return TimeSpan.FromSeconds(seconds);
+        } 
+    }
+}
