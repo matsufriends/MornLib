923236b3a1f70745defbf50e5810db0e0c00d5e3
matsu_friends <pikapikan0526@gmail.com>
2024-06-02 15:22:57 +0900

    [feat] StartDspTimeを公開

--- MornBeat/Runtime/MornBeatControllerMono.cs
+++ MornBeat/Runtime/MornBeatControllerMono.cs
@@ -28,6 +28,7 @@ namespace MornBeat
         public int BeatCount => _currentBeatMemo.BeatCount;
         public int BeatTick => MeasureTickCount / BeatCount;
         public double CurrentBeatLength => 60d / CurrentBpm;
+        public double StartDspTime => _startDspTime;
         /// <summary> ループ時に0から初期化 </summary>
         public double MusicPlayingTime => AudioSettings.dspTime - _loopStartDspTime + (_currentBeatMemo != null ? _currentBeatMemo.Offset : 0) + _offsetTime;
         /// <summary> ループ後に値を継続 </summary>
