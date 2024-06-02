6ebd47e0aea800bd73700f167537eb8de70092f6
matsu_friends <pikapikan0526@gmail.com>
2024-06-02 16:37:33 +0900

    [feat] 編集時もサイズが即反映するよう修正

--- MornCamera/Runtime/MornCameraAdjusterMono.cs
+++ MornCamera/Runtime/MornCameraAdjusterMono.cs
@@ -1,4 +1,7 @@
 ﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace MornCamera
 {
@@ -13,6 +16,7 @@ namespace MornCamera
         private void Start()
         {
             cachedRes = new Vector2(Screen.width, Screen.height);
+            AdjustCamera(cachedRes);
         }
 
         private void Update()
@@ -29,14 +33,27 @@ namespace MornCamera
         {
             var currentAspect = screenRes.y / screenRes.x;
             var aimAspect = cameraSetting.Resolution.y / cameraSetting.Resolution.x;
+            Rect newRect;
             if (currentAspect > aimAspect)
             {
                 var gameRes = new Vector2(screenRes.x, screenRes.x * aimAspect);
-                targetCamera.rect = new Rect(0, (screenRes.y - gameRes.y) / screenRes.y / 2, 1, gameRes.y / screenRes.y);
+                newRect= new Rect(0, (screenRes.y - gameRes.y) / screenRes.y / 2, 1, gameRes.y / screenRes.y);
             }
             else
             {
-                targetCamera.rect = new Rect(0, 0, 1, 1);
+                newRect = new Rect(0, 0, 1, 1);
+            }
+
+            var curRect = targetCamera.rect;
+            if (curRect != newRect)
+            {
+                targetCamera.rect = newRect;
+#if UNITY_EDITOR
+                if (!Application.isPlaying)
+                {
+                    EditorUtility.SetDirty(targetCamera);
+                }
+#endif
             }
         }
 
