c2b4c4815bdcd2f95dc3481f80f677cd3c8bf732
matsu_friends <pikapikan0526@gmail.com>
2024-06-02 15:20:57 +0900

    [feat] アス比に合わせてカメラサイズ調整

---
+++ MornCamera/Runtime/MornCameraAdjusterMono.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace MornCamera
+{
+    [RequireComponent(typeof(Camera))]
+    [ExecuteAlways]
+    public sealed class MornCameraAdjusterMono : MonoBehaviour
+    {
+        [SerializeField] private Camera targetCamera;
+        [SerializeField] private MornCameraSettingSo cameraSetting;
+        private Vector2 cachedRes;
+
+        private void Start()
+        {
+            cachedRes = new Vector2(Screen.width, Screen.height);
+        }
+
+        private void Update()
+        {
+            var currentRes = new Vector2(Screen.width, Screen.height);
+            if (cachedRes != currentRes)
+            {
+                cachedRes = currentRes;
+                AdjustCamera(currentRes);
+            }
+        }
+
+        private void AdjustCamera(Vector2 screenRes)
+        {
+            var currentAspect = screenRes.y / screenRes.x;
+            var aimAspect = cameraSetting.Resolution.y / cameraSetting.Resolution.x;
+            if (currentAspect > aimAspect)
+            {
+                var gameRes = new Vector2(screenRes.x, screenRes.x * aimAspect);
+                targetCamera.rect = new Rect(0, (screenRes.y - gameRes.y) / screenRes.y / 2, 1, gameRes.y / screenRes.y);
+            }
+            else
+            {
+                targetCamera.rect = new Rect(0, 0, 1, 1);
+            }
+        }
+
+        private void OnValidate()
+        {
+            if (targetCamera == null)
+            {
+                targetCamera = GetComponent<Camera>();
+            }
+        }
+    }
+}
---
+++ MornCamera/Runtime/MornCameraSettingSo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MornCamera
+{
+    [CreateAssetMenu(fileName = nameof(MornCameraSettingSo), menuName = nameof(MornCameraSettingSo))]
+    public sealed class MornCameraSettingSo : ScriptableObject
+    {
+        public Vector2 Resolution = new(1920, 1080);
+    }
+}
