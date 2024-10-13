009f995ba415abc57c0fb60f7b23869abf3231ce
matsufriends <pikapikan0526@gmail.com>
2024-10-13 09:32:00 +0900

    SpriteのOrderをHierarchyに表示するクンの復活

---
+++ MornHierarchySpriteOrderDrawer.cs
@@ -0,0 +1,66 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace PastelParade
+{
+    public static class MornHierarchySpriteOrderDrawer
+    {
+        [InitializeOnLoadMethod]
+        private static void AddHierarchyItemOnGUI()
+        {
+            EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
+        }
+
+        private static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
+        {
+            var gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
+            if (gameObject == null) return;
+            DrawTag(selectionRect, gameObject);
+        }
+
+        private static void DrawTag(Rect selectionRect, GameObject gameObject)
+        {
+            var sortingGroup = gameObject.GetComponentInParent<SortingGroup>(true);
+            var renderer = gameObject.GetComponent<Renderer>();
+            if (renderer == null)
+            {
+                return;
+            }
+
+            var rendererText = $"{LayerNameToNumber(renderer.sortingLayerName)}-{renderer.sortingOrder}";
+            var text = "";
+            if (sortingGroup != null)
+            {
+                text = $"{LayerNameToNumber(sortingGroup.sortingLayerName)}-{sortingGroup.sortingOrder}({rendererText})";
+            }
+            else
+            {
+                text = rendererText;
+            }
+
+            var style = new GUIStyle();
+            selectionRect.xMax -= 16;
+            selectionRect.xMin = selectionRect.xMax - 80;
+            style.normal.textColor = GUI.color;
+            style.alignment = TextAnchor.MiddleRight;
+            EditorGUI.LabelField(selectionRect, text, style);
+        }
+
+        private static string LayerNameToNumber(string layerName)
+        {
+            var numberCount = 0;
+            for (var i = layerName.Length - 1; i >= 0; i--)
+            {
+                if (!char.IsDigit(layerName[i]))
+                {
+                    break;
+                }
+
+                numberCount++;
+            }
+
+            return layerName[^numberCount..];
+        }
+    }
+}
